2be11959f0be0cc39a9b99e4319832e3dd4679e6
bayarookie <bayarookie@bk.ru>
2016-10-21 01:57:06 +0800

    5
    report

--- report.php
+++ report.php
@@ -1,26 +1,42 @@
 <?php
 include 'db.php';
-echo '<article><h1><a href="">Отчёт</a></h1>
-<table><tr><th>Группа<th>Сумма';
+echo '<article><h1><a href="">Отчёт</a></h1>';
+echo '<style>
+.report {
+	padding: 10px;
+	display: inline-block;
+	min-width: 175px;
+	margin: 0 10px 10px 0;
+}
+figcaption {
+	text-align: center;
+}
+</style>';
 $query = "SELECT DATE_FORMAT(op_date,'%Y-%m') as mo, groups.id, groups.name, SUM(op_summ) as summ"
 		." FROM money"
 		." LEFT JOIN goods ON money.goods_id=goods.id"
 		." LEFT JOIN groups ON goods.groups_id=groups.id"
-		." GROUP BY mo, groups.id";
+		." GROUP BY mo, groups.id ORDER BY mo, groups.name";
 $result = byQu($mysqli, $query);
 $mo = "";
 $sm = 0;
 while ($row = $result->fetch_assoc()) {
 	if ($mo != $row['mo']) {
-		if ($mo != "") echo '<tr class="minus"><td>' . $mo . '<td align="right">' . $sm;
+		if ($mo != "") {
+			if ($sm < 0) {echo '<tr class="minus">';} else {echo '<tr class="plus">';}
+			echo '<td>Итого<td align="right">' . $sm;
+			echo '</table></figure>';
+		}
 		$mo = $row['mo'];
 		$sm = floatval($row['summ']);
-		echo '<tr class="plus"><td colspan=2>' . $mo;
+		echo '<figure class="report"><figcaption>' . $mo . '</figcaption><table><tr><th>Группа<th>Сумма';
 	} else $sm = $sm + floatval($row['summ']);
-	echo '<tr><td>' . $row['name'] . '<td align="right">
+	if (floatval($row['summ']) < 0) {echo '<tr class="minus">';} else {echo '<tr class="plus">';}
+	echo '<td>' . $row['name'] . '<td align="right">
 <input type="button" value="' . $row['summ'] . '" onclick="money_table(2,\'' . $row['mo'] . '\',' . $row['id'] . ')">';
 }
-echo '<tr class="minus"><td>' . $mo . '<td align="right">' . $sm;
-echo '</table>';
+if ($sm < 0) {echo '<tr class="minus">';} else {echo '<tr class="plus">';}
+echo '<td>Итого<td align="right">' . $sm;
+echo '</table></figure>';
 ?>
 <input type="button" value="Закрыть" onclick="id_close('report')"></article>
